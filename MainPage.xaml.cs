ac96119fce2c82aca8c05d9dd00f575a30b16f38
irreversible <irreversible>
2015-10-22 15:25:41 +0600

    Windows Phone 8 app
    This is a windows phone 8 app that would be using accelerometer  , and
    playing a media file . A event would be called and that would play the
    file .

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ShakeItShakeIt.Resources;
+using ShakeGestures;
+
+namespace ShakeItShakeIt
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+            ShakeGesturesHelper.Instance.ShakeGesture += new EventHandler<ShakeGestureEventArgs>(Perform_Shakeevent);
+
+
+            //set the no of shake parameter after which you want shake event handler to fire
+            ShakeGesturesHelper.Instance.MinimumRequiredMovesForShake = 3;
+
+            //Activate the shake gesture helper
+            ShakeGesturesHelper.Instance.Active = true;
+        }
+        private void Perform_Shakeevent(object sender, ShakeGestureEventArgs e)
+        {
+            //Enclose the task you need to perform in the following dispatcher code
+            Dispatcher.BeginInvoke(() =>
+            {
+               // displayblock.Text = DateTime.Now.ToLongTimeString();
+                Media.Play();
+            });
+        }
+        
+        
+    }
+}
